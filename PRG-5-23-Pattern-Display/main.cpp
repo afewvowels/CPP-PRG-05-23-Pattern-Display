12207df52fe631988d190596db9ddb417c5e07a5
Keith Smith <aeiouandsometimesy@me.com>
2017-10-07 17:49:58 -0400

    Initial Commit

---
+++ PRG-5-23-Pattern-Display/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PRG-5-23-Pattern-Display
+//
+//  Created by Keith Smith on 10/7/17.
+//  Copyright © 2017 Keith Smith. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
