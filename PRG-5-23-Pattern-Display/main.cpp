0dba717f0d10e847c5f03dc8264a60f8475e2111
Keith Smith <aeiouandsometimesy@me.com>
2017-10-07 17:53:02 -0400

    Added comments.

--- PRG-5-23-Pattern-Display/main.cpp
+++ PRG-5-23-Pattern-Display/main.cpp
@@ -5,11 +5,59 @@
 //  Created by Keith Smith on 10/7/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//
+//  Write a program that uses a loop to display Pattern A below, followed by another loop
+//  that displays Pattern B.
+//
+//  PATTERN A
+//  +
+//  ++
+//  ...
+//  ++++++++++
+//
+//  PATTERN B
+//  ++++++++++
+//  +++++++++
+//  ...
+//  +
+
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main()
+{
+    // Declare and initialize variables
+    int intPatternSize = 10,
+        intPatternCount = 0;
+    
+    for(int a = 0 ; a <intPatternSize ; a++)
+    {
+        // Increment to get increasing number of X's
+        for(int b = 0 ; b <= intPatternCount ; b++)
+        {
+            cout << 'X';
+        }
+        intPatternCount++;
+        cout << endl;
+    }
+    
+    // Divide two patterns
+    cout << endl << endl;
+    
+    for(int i = 0 ; i < intPatternSize ; i++)
+    {
+        // Decrement to get decreasing number of X's
+        for(int k = intPatternCount ; k > 0 ; k--)
+        {
+            cout << 'X';
+        }
+        intPatternCount--;
+        cout << endl;
+    }
+    
     return 0;
 }
+
+
